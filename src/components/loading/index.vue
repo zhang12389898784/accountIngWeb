28deb6ec0bc6c35d6af4388cc3a89fe6fca90193
zhangchenxi <2564930650@qq.com>
2025-01-18 00:12:17 +0800

    feat(components): 添加加载组件并更新 404 页面
    - 新增加载组件，包含模板、样式和动画
    - 在 404 页面中引入加载组件

---
+++ src/components/loading/index.vue
@@ -0,0 +1,98 @@
+<template>
+<div style="width: 100%;justify-content: center;align-items: center;display: flex;">
+    <div class="loading">
+    <div v-for="item in 36" class="dot">
+    </div>
+    </div>
+</div>
+</template>
+<script setup lang="ts">
+
+</script>
+<style lang="scss" scoped>
+$ballSize: 10px;
+$containerSize: 150px;
+$animationDuration: 3s;
+.loading {
+    width: $containerSize;
+    height: $containerSize;
+    margin-top: 50px auto;
+    position:relative;
+    border-radius: 50%;
+    .dot{
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        width: $ballSize;
+        height: $ballSize;
+        margin-left: -$ballSize/2;
+        margin-top: -$ballSize/2;
+        perspective: 100px;
+        transform-style: preserve-3d;
+        &::after{
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: rgb(14, 128, 234);
+            top: 100%;
+            animation:moveWhite $animationDuration  infinite;
+        }
+        &::before{
+            content: '';
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            background-color: black;
+            top: -100%;
+            animation:moveBlack $animationDuration  infinite;
+        }
+    }
+}
+@for $i from 1 through 36 {
+    .dot:nth-child(#{$i}){
+    transform:rotate((15deg)*$i) translateY(-$containerSize/2);
+    &::after,&::before{
+        animation-delay:  calc(#{$animationDuration} * #{$i} / 36*6);
+    }
+}
+}
+@keyframes moveBlack{
+    0%{
+        animation-timing-function: ease-in;
+    }
+    25%{
+        transform: translate3d(0,100%, $ballSize);
+        animation-timing-function: ease-out;
+    }
+    50%{
+        transform: translate3d(0,200%, 0);
+        animation-timing-function: ease-in;
+    }
+    75%{
+        transform: translate3d(0,100%, $ballSize);
+        animation-timing-function: ease-out;
+    }
+
+}
+@keyframes moveWhite{
+    0%{
+        animation-timing-function: ease-in;
+    }
+    25%{
+        transform: translate3d(0,-100%, $ballSize);
+        animation-timing-function: ease-out;
+    }
+    50%{
+        transform: translate3d(0,-200%, 0);
+        animation-timing-function: ease-in;
+    }
+    75%{
+        transform: translate3d(0,-100%, $ballSize);
+        animation-timing-function: ease-out;
+    }
+
+}
+</style>
--- src/pages/error/404.vue
+++ src/pages/error/404.vue
@@ -1,3 +1,7 @@
 <template>
     404
+    <Loading />
 </template>
+<script setup lang="ts">
+import Loading from '@/components/loading/index.vue'
+</script>
